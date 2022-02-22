f3d3b1e44534dee1c0b0dc3ffae51ae5da505fa3
ebuka <peterdbrainy5@gmail.com>
2022-02-22 06:11:25 +0100

    Add the basic React code structure

---
+++ views/Auth/SignUpView.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+const SignUpView = () => {
+
+    return (
+        <View>
+            <Text>Signup view</Text>
+        </View>
+    )
+}
+
+
+export default SignUpView;
